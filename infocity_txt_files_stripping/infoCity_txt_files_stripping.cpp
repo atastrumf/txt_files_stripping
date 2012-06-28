29cb0a4105d2d3dd306f3efa707bb8f9d4e3d669
Atastrumf <nejcmales@gmail.com>
2012-06-28 16:10:34 +0200

    added boost supported directory searching

--- infocity_txt_files_stripping/infoCity_txt_files_stripping.cpp
+++ infocity_txt_files_stripping/infoCity_txt_files_stripping.cpp
@@ -1,10 +1,45 @@
 // infoCity_txt_files_stripping.cpp : Defines the entry point for the console application.
 //
 
+#include "stdafx.h"
+
 #include <iostream>
+#include <string>
+#include <vector>
+#include <boost\filesystem.hpp>
+
+void getFileNames(std::string directoryName, std::vector<std::string>& fileNames);
+
 int main()
 {
+	std::vector<std::string> fileNames;
+
+	std::string directoryName;
+	std::cout << "Enter directory path: ";
+	std::cin >> directoryName;
+	getFileNames("d:/test/", fileNames);
+
+	for(std::vector<std::string>::iterator it = fileNames.begin(); it != fileNames.end(); ++it)
+		std::cout << *it << std::endl;
+
 	std::cin.get();
 	return 0;
 }
 
+// search directory for txt files with boost
+void getFileNames(std::string directoryName, std::vector<std::string>& fileNames)
+{
+	// transform directoryName to path
+	boost::filesystem::path path(directoryName);
+
+	boost::filesystem::directory_iterator end_it;
+	for(boost::filesystem::directory_iterator it(path); it != end_it; ++it)
+	{
+		// skip if not a file
+		if(!boost::filesystem::is_regular_file(it->status())) continue;
+
+		// if file is txt than it is stored
+		if(it->path().extension() == ".txt")
+			fileNames.push_back(it->path().filename().string());
+	}
+}
