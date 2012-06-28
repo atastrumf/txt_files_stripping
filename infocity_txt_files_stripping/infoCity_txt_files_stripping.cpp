9f4903ba1135120db58c98564e86238da41fc447
Atastrumf <nejcmales@gmail.com>
2012-06-28 17:15:44 +0200

    added function for reading files

--- infocity_txt_files_stripping/infoCity_txt_files_stripping.cpp
+++ infocity_txt_files_stripping/infoCity_txt_files_stripping.cpp
@@ -7,8 +7,10 @@
 #include <string>
 #include <vector>
 #include <boost\filesystem.hpp>
+#include <boost\filesystem\fstream.hpp>
 
 void getFileNames(std::string directoryName, std::vector<std::string>& fileNames);
+std::string readFile(std::string directoryName, std::string fileName);
 
 int main()
 {
@@ -16,11 +18,14 @@ int main()
 
 	std::string directoryName;
 	std::cout << "Enter directory path: ";
-	std::cin >> directoryName;
-	getFileNames("d:/test/", fileNames);
+	//std::cin >> directoryName;
+	directoryName = "d:/test/";
+	getFileNames(directoryName, fileNames);
 
 	for(std::vector<std::string>::iterator it = fileNames.begin(); it != fileNames.end(); ++it)
 		std::cout << *it << std::endl;
+
+	std::string file = readFile(directoryName, fileNames[0]);
 
 	std::cin.get();
 	return 0;
@@ -41,5 +46,19 @@ void getFileNames(std::string directoryName, std::vector<std::string>& fileNames
 		// if file is txt than it is stored
 		if(it->path().extension() == ".txt")
 			fileNames.push_back(it->path().filename().string());
+	}
+}
+
+// reading file and returning it as a std::string
+std::string readFile(std::string directoryName, std::string fileName)
+{
+	// using boost ifstream, but with std::ifstream would be the same
+	boost::filesystem::ifstream inputFile(directoryName + fileName);
+	if(!inputFile.good())
+		return "ERROR: cannot open file " + fileName;
+	else
+	{
+		std::string file(std::istreambuf_iterator<char>( inputFile ), (std::istreambuf_iterator<char>()));
+		return file;
 	}
 }
